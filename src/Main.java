5446521ce97b1f643a3436c4ca8a6b1b24f71e4a
Sindija <sindijabarzdina@inbox.lv>
2022-04-05 10:32:26 +0200

    ieliku komentaru

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         loop: while (true) {
 
             choiseStr = sc.next();
-//eeresczsczsczdzszkk
+//komentars
             switch (choiseStr) {
                 case "comp":
                     System.out.print("source file name: ");
