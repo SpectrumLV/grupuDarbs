f843dedda307c47d5d223621c6d6faa960d54a9b
Dima <dmitrijs.sizovs@gmail.com>
2022-04-27 20:12:22 +0300

    parbaudee

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
             choiseStr = sc.next();
 //komentars
-// halo
+//parbaude
             switch (choiseStr) {
                 case "comp":
                     System.out.print("source file name: ");
